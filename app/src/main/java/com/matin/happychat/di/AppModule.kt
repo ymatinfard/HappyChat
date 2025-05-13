da365750d18b0a254074a2b3fe6dfeec9917cd6b
Yousef <matinfard.y@gmail.com>
2025-05-13 17:41:58 +0200

    Add Coroutine Dispatchers

--- app/src/main/java/com/matin/happychat/di/AppModule.kt
+++ app/src/main/java/com/matin/happychat/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.matin.happychat.data.grpc.GRPCClient
 import com.matin.happychat.data.grpc.GrpcChatRepository
 import com.matin.happychat.data.grpc.GrpcGrpcChatRepositoryImpl
 import com.matin.happychat.data.local.ChatDatabase
+import com.matin.happychat.data.local.MessageDao
 import com.matin.happychat.domain.MessageRepository
 import com.matin.happychat.mediaplayer.AudioFileStorage
 import com.matin.happychat.mediaplayer.ExternalAudioFileStorage
@@ -16,6 +17,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
 import javax.inject.Singleton
 
 @Module
@@ -30,11 +34,23 @@ object AppProvidesModule {
 
     @Provides
     @Singleton
-    fun provideDb(@ApplicationContext applicationContext: Context) = Room.databaseBuilder(
+    fun provideDb(@ApplicationContext applicationContext: Context): ChatDatabase = Room.databaseBuilder(
         applicationContext,
         ChatDatabase::class.java,
         "chat_database"
     ).build()
+
+    @Provides
+    @Singleton
+    fun provideMessageDao(chatDatabase: ChatDatabase): MessageDao = chatDatabase.messageDao()
+
+    @Provides
+    @IoDispatcher
+    fun provideCoroutineIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @MainDispatcher
+    fun provideCoroutineMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 }
 
 @Module
@@ -49,3 +65,11 @@ interface AppBindsModule {
     @Singleton
     fun bindMessageRepository(messageRepositoryImpl: MessageRepositoryImpl): MessageRepository
 }
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
